3cab4f9df0f482eedf3057d90a7a80c3659f866f
Santiago <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-24 16:28:13 -0700

    Added EjemploHashSet.java implementation
    Added a HashSet example demonstrating the use of Set in Java.

--- src/EjemploHashSet.java
+++ src/EjemploHashSet.java
@@ -1,1 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
 
+public class EjemploHashSet {
+    public static void main(String[] args) {
+        Set<String> tecnologias = new HashSet<>();
+
+        tecnologias.add("Java");
+        tecnologias.add("Python");
+        tecnologias.add("JavaScript");
+        tecnologias.add("Java");
+        tecnologias.add("SQL");
+        tecnologias.add("Python");
+        tecnologias.add("Git");
+        tecnologias.add("Docker");
+
+        System.out.println(tecnologias);
+    }
+}
