8396cb7c6a92075a8eb54b78f4d6f711b3cd5c34
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-25 11:15:38 -0700

    Added implementation

--- src/EjemploTreeSet.java
+++ src/EjemploTreeSet.java
@@ -1,1 +1,20 @@
+import java.util.Set;
+import java.util.TreeSet;
 
+public class EjemploTreeSet {
+    public static void main(String[] args) {
+        Set<String> tecnologias = new TreeSet<>();
+
+        tecnologias.add("Java");
+        tecnologias.add("Python");
+        tecnologias.add("JavaScript");
+        tecnologias.add("Java");
+        tecnologias.add("SQL");
+        tecnologias.add("Git");
+        tecnologias.add("Docker");
+
+        for (String tecnologia : tecnologias) {
+            System.out.println(tecnologia);
+        }
+    }
+}
