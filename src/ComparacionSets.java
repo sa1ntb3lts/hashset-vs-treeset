cb97d97f73dd601f85d6ab4b4619fa41fc4575a0
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-25 11:19:30 -0700

    Added implementation to ComparacionSets.java

--- src/ComparacionSets.java
+++ src/ComparacionSets.java
@@ -1,1 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
+public class ComparacionSets {
+    public static void main(String[] args) {
+        probarSet("HashSet", new HashSet<>());
+        probarSet("TreeSet", new TreeSet<>());
+    }
+
+    private static void probarSet(String nombre, Set<String> conjunto) {
+        conjunto.add("Python");
+        conjunto.add("Java");
+        conjunto.add("SQL");
+        conjunto.add("Docker");
+        conjunto.add("Java");
+        conjunto.add("Git");
+
+        System.out.println(nombre);
+        System.out.println(conjunto);
+        System.out.println("Contiene Java: " + conjunto.contains("Java"));
+        System.out.println("Tamaño: " + conjunto.size());
+    }
+}
