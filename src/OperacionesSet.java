7d43c2310306f613fec853bad33f7bc395d8049d
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-25 11:41:07 -0700

    Update OperacionesSet.java

--- src/OperacionesSet.java
+++ src/OperacionesSet.java
@@ -1,1 +1,20 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
+public class OperacionesSet {
+    public static void main(String[] args) {
+        TreeSet<Integer> calificaciones = new TreeSet<>();
+
+        calificaciones.add(65);
+        calificaciones.add(70);
+        calificaciones.add(75);
+        calificaciones.add(80);
+        calificaciones.add(85);
+        calificaciones.add(90);
+        calificaciones.add(95);
+
+        System.out.println("Mínimo: " + calificaciones.first());
+        System.out.println("Máximo: " + calificaciones.last());
+    }
+}
